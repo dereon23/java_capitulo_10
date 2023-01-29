dd218ab397fe814dde14bf206e31676fb1bb6483
dereon23 <115071403+dereon23@users.noreply.github.com>
2023-01-29 18:02:16 +0100

    Add files via upload

---
+++ Ejercicio19.java
@@ -0,0 +1,62 @@
+package Capitulo10;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Ejercicio19 {
+	
+	public static void main(String[] args) {
+		HashMap<String, String> dicc = new HashMap<String,String>();
+		ArrayList<String> sinom = new ArrayList<String>();
+		dicc.put("caliente", "hot");
+		dicc.put("rojo", "red");
+		dicc.put("ardiente", "hot");
+		dicc.put("verde", "green");
+		dicc.put("agujetas", "stiff");
+		dicc.put("abrasador", "hot");
+		dicc.put("hierro", "iron");
+		dicc.put("grande", "big");
+		
+		Scanner s= new Scanner(System.in);
+		String entrada="";
+		while(!entrada.equals("salir")) {
+			System.out.print("\nIntroduzca una palabra y le daré los sinónimos: ");
+			entrada=s.nextLine();
+			sinom.clear();
+			if(!entrada.equals("salir")) {
+			if(dicc.containsKey(entrada)) {
+			for(Map.Entry<String,String> par : dicc.entrySet()) {
+				if(par.getKey().equals(entrada)) {
+					for(Map.Entry<String, String> par2 : dicc.entrySet()) {
+						if(par2.getValue().equals(par.getValue())&&!par2.getKey().equals(entrada)) {
+							sinom.add(par2.getKey());
+						}
+					}
+					break;
+				}
+			}
+			if(sinom.size()>0) {
+			System.out.print("Sinónimos de "+entrada+": ");
+			for(int i=0; i<sinom.size();i++) {
+				if(i<sinom.size()-1) {
+				System.out.print(sinom.get(i)+", ");
+				}else {
+					System.out.println(sinom.get(i));
+				}
+			}
+			}else {
+				System.out.println("No conozco sinónimos de esa palabra");
+
+			}
+			}else {
+				System.out.println("No conozco esa palabra");
+			}
+			}
+		}
+		
+
+	}
+
+}
